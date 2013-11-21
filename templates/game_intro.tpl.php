da1e246d504f980674b9df8c27cfc307d0aa5ea9
Erin McLaughlin <emclaughlin@digitalbungalow.com>
2013-11-21 12:20:20 -0500

    Adding a containing div.

--- templates/game_intro.tpl.php
+++ templates/game_intro.tpl.php
@@ -1,9 +1,11 @@
-<div class="game-list">
-  <h1 class="game-list-title">Choose Your Game</h1>
-  <ul class="game-nodes">
-    <?php foreach ($nodes as $node): ?>
-      <li class="game-node" data-node="<?= $node->nid ?>"><?= $node->field_qa_game_account_code['und'][0]['value'] ?> - <?= $node->title ?></li>
-    <?php endforeach; ?>
-  </ul>
-  <h1 class="game-start">Start</h1>
+<div class="game">
+  <div class="game-list">
+    <h1 class="game-list-title">Choose Your Game</h1>
+    <ul class="game-nodes">
+      <?php foreach ($nodes as $node): ?>
+        <li class="game-node" data-node="<?= $node->nid ?>"><?= $node->field_qa_game_account_code['und'][0]['value'] ?> - <?= $node->title ?></li>
+      <?php endforeach; ?>
+    </ul>
+    <h1 class="game-start">Start</h1>
+  </div>
 </div>
